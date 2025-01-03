04a2ed979dda62cac3646cfd2c86a5c3b466dfb1
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2025-01-03 12:31:50 +0300

    make google search func

---
+++ bs_parser.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def search_google(query, num_results=5):
+    url = f"https://www.google.com/search?q={query}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    results = []
+    for g in soup.find_all('div', class_='tF2Cxc'):
+        link = g.find('a')['href']
+        title = g.find('h3').text
+        results.append({'title': title, 'link': link})
+        if len(results) >= num_results:
+            break
+    return results
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+
 from dotenv import load_dotenv
 import os
+
+dot_env_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dot_env_path):
+    load_dotenv(dot_env_path)
+
+bot = Bot(token=os.getenv('BOT_TOKEN'))
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: Message):
+    await message.reply(
+        "Привет, я бот, который поможет "
+        "тебе найти нужную информацию о фильме"
+        )
+
+
